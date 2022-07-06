e779519f76d61ee4c4b14ff15d45fa7a2485d0d2
Andyyy228 <anton.bot1212@gmail.com>
2022-07-07 01:11:51 +0300

    End of section 6

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,24 +1,21 @@
 import { ShoppingCart } from "@mui/icons-material";
-import { AppBar, Badge, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
-import { Box } from "@mui/system";
-import { NavLink } from "react-router-dom";
+import { AppBar, Badge, Box, IconButton, List, ListItem, Switch, Toolbar, Typography } from "@mui/material";
+import { Link, NavLink } from "react-router-dom";
+import { useStoreContext } from "../context/StoreContext";
 
 interface Props {
     darkMode: boolean;
     handleThemeChange: () => void;
 }
-
 const midLinks = [
     { title: 'catalog', path: '/catalog' },
     { title: 'about', path: '/about' },
-    { title: 'contact', path: '/contact' },
+    { title: 'contact', path: '/contact' }
 ]
-
 const rightLinks = [
     { title: 'login', path: '/login' },
-    { title: 'register', path: '/register' },
+    { title: 'register', path: '/register' }
 ]
-
 const navStyles = {
     color: 'inherit',
     textDecoration: 'none',
@@ -32,20 +29,19 @@ const navStyles = {
 }
 
 export default function Header({ darkMode, handleThemeChange }: Props) {
-    return (
-        <AppBar position="static" sx={{ mb: 4 }}>
-            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+    const { basket } = useStoreContext();
+    const itemCount = basket?.items.reduce((sum, item) => sum + item.quantity, 0)
 
+    return (
+        <AppBar position='static' sx={{ mb: 4 }}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
                 <Box display='flex' alignItems='center'>
-                    <Typography variant='h6' component={NavLink}
-                        to='/'
-                        exact
+                    <Typography variant='h6' component={NavLink} exact to='/'
                         sx={navStyles}>
                         RE-STORE
                     </Typography>
                     <Switch checked={darkMode} onChange={handleThemeChange} />
                 </Box>
-
                 <List sx={{ display: 'flex' }}>
                     {midLinks.map(({ title, path }) => (
                         <ListItem
@@ -58,10 +54,9 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
                         </ListItem>
                     ))}
                 </List>
-
                 <Box display='flex' alignItems='center'>
-                    <IconButton size='large' sx={{ color: 'inherit' }}>
-                        <Badge badgeContent={4} color='secondary'>
+                    <IconButton component={Link} to='/basket' size='large' sx={{ color: 'inherit' }}>
+                        <Badge badgeContent={itemCount} color='secondary'>
                             <ShoppingCart />
                         </Badge>
                     </IconButton>
@@ -78,7 +73,6 @@ export default function Header({ darkMode, handleThemeChange }: Props) {
                         ))}
                     </List>
                 </Box>
-
             </Toolbar>
         </AppBar>
     )
