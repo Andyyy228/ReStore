100c55eab02c4899f891482ee79a5a4f89d262d0
Andyyy228 <anton.bot1212@gmail.com>
2022-07-17 15:08:53 +0300

    End of section 8

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,7 +7,7 @@ import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { Provider } from 'react-redux';
 import { store } from './app/store/configureStore';
-
+ 
 export const history = createBrowserHistory();
 
 const root = ReactDOM.createRoot(
