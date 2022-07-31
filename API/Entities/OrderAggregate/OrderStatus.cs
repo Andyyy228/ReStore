2e1f50e6d89aa1eacb4d16cefe632355a3347bc0
Andyyy228 <anton.bot1212@gmail.com>
2022-07-31 20:00:55 +0300

    End of section 10

---
+++ API/Entities/OrderAggregate/OrderStatus.cs
@@ -0,0 +1,9 @@
+namespace API.Entities.OrderAggregatem
+{
+    public enum OrderStatus
+    {
+        Pending,
+        PaymentReceived,
+        PaymentFailed
+    }
+}
--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,17 +1,17 @@
 import { LoadingButton } from "@mui/lab";
 import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router";
 import NotFound from "../../app/errors/NotFound";
 import LoadingComponent from "../../app/layout/LoadingComponent";
 import { useAppDispatch, useAppSelector } from "../../app/store/configureStore";
-import { addBasketItemAsync, removeBasketItemAsync } from "../basket/basketSlice";
+import { addBasketItemAsync, removeBasketItemAsync  } from "../basket/basketSlice";
 import { fetchProductAsync, productSelectors } from "./catalogSlice";
 
 export default function ProductDetails() {
-    const { basket, status} = useAppSelector(state => state.basket);
+    const {basket, status} = useAppSelector(state => state.basket);
     const dispatch = useAppDispatch();
-    const { id } = useParams<{ id: string }>();
+    const {id} = useParams<{id: string}>();
     const product = useAppSelector(state => productSelectors.selectById(state, id));
     const {status: productStatus} = useAppSelector(state => state.catalog);
     const [quantity, setQuantity] = useState(0);
@@ -20,10 +20,10 @@ export default function ProductDetails() {
     useEffect(() => {
         if (item) setQuantity(item.quantity);
         if (!product) dispatch(fetchProductAsync(parseInt(id)))
-    }, [id, item, dispatch, product])
+    }, [id, item, dispatch, product]);
 
     function handleInputChange(event: any) {
-        if (event.target.value >= 0) {
+        if (event.target.value > 0) {
             setQuantity(parseInt(event.target.value));
         }
     }
@@ -45,11 +45,11 @@ export default function ProductDetails() {
     return (
         <Grid container spacing={6}>
             <Grid item xs={6}>
-                <img src={product.pictureUrl} alt={product.name} style={{ width: '100%' }} />
+                <img src={product.pictureUrl} alt={product.name} style={{width: '100%'}} />
             </Grid>
             <Grid item xs={6}>
                 <Typography variant='h3'>{product.name}</Typography>
-                <Divider sx={{ mb: 2 }} />
+                <Divider sx={{mb: 2}} />
                 <Typography variant='h4' color='secondary'>${(product.price / 100).toFixed(2)}</Typography>
                 <TableContainer>
                     <Table>
@@ -57,43 +57,43 @@ export default function ProductDetails() {
                             <TableRow>
                                 <TableCell>Name</TableCell>
                                 <TableCell>{product.name}</TableCell>
-                            </TableRow>
+                            </TableRow>    
                             <TableRow>
                                 <TableCell>Description</TableCell>
                                 <TableCell>{product.description}</TableCell>
-                            </TableRow>
+                            </TableRow>  
                             <TableRow>
                                 <TableCell>Type</TableCell>
                                 <TableCell>{product.type}</TableCell>
-                            </TableRow>
+                            </TableRow>  
                             <TableRow>
                                 <TableCell>Brand</TableCell>
                                 <TableCell>{product.brand}</TableCell>
-                            </TableRow>
+                            </TableRow>  
                             <TableRow>
                                 <TableCell>Quantity in stock</TableCell>
                                 <TableCell>{product.quantityInStock}</TableCell>
-                            </TableRow>
+                            </TableRow>  
                         </TableBody>
                     </Table>
                 </TableContainer>
                 <Grid container spacing={2}>
                     <Grid item xs={6}>
-                        <TextField
-                            onChange={handleInputChange}
+                        <TextField 
                             variant='outlined'
                             type='number'
                             label='Quantity in Cart'
                             fullWidth
                             value={quantity}
+                            onChange={handleInputChange}
                         />
                     </Grid>
                     <Grid item xs={6}>
                         <LoadingButton
-                            disabled={item?.quantity === quantity || !item && quantity === 0}
+                            disabled={item?.quantity === quantity}
                             loading={status.includes('pending')}
                             onClick={handleUpdateCart}
-                            sx={{ height: '55px' }}
+                            sx={{height: '55px'}}
                             color='primary'
                             size='large'
                             variant='contained'
@@ -106,4 +106,4 @@ export default function ProductDetails() {
             </Grid>
         </Grid>
     )
-} 
+}
